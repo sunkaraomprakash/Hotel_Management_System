a0cb9616f54635517c23c926f58244a90587abb5
Lenovo <Lenovo@DESKTOP-O68I67T>
2022-04-07 12:28:42 +0530

    Add project files.

---
+++ Hotel_Management_System/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+namespace Hotel_Management_System.Models
+{
+    public class User
+    {
+        
+      
+        public int UserId { get; set; }
+        
+        public string WorkDomain { get; set; }
+     
+        public string UserName { get; set; }
+      
+
+        public string Password { get; set; }
+     
+
+        public string RePassword { get; set; }
+    }
+}
